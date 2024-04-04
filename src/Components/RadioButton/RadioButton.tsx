d68056d194235c4b64ea74dc40fc32a0bec00772
Michaela <michaelachan45@gmail.com>
2024-04-04 08:56:07 +0100

    made filter buttons selectable and deselectable

--- src/Components/RadioButton/RadioButton.tsx
+++ src/Components/RadioButton/RadioButton.tsx
@@ -12,7 +12,7 @@ type RadioButtonProps = {
 const RadioButton = ({label, value, groupName,  handleChange}: RadioButtonProps) => {
   return(
     <div className="radio-button">
-      <input onChange={handleChange} className="radio-button__input" type="radio" id={label} name={groupName} value={value}/>
+      <input onChange={handleChange} className="radio-button__input" type="checkbox" id={label} name={groupName} value={value}/>
       <label className="radio-button__label" htmlFor={label}>{label}</label>
 
     </div>
